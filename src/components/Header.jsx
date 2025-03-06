bb5bc6cffb59d01e3b34ed9acbcb4c17395c1be4
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-03-06 22:05:37 +0530

    add sorting functionality to Header component with order and criteria selection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useFolder } from "../hooks/useFolder.js";
-import { setSearchData } from "../store/folderSlice.js";
+import {
+  setSearchData,
+  setFolderData,
+  setSortData,
+} from "../store/folderSlice.js";
 
 export default function Header() {
   const [isClickNewFolder, setIsClickNewFolder] = useState(false);
   const [newFolderName, setNewFolderName] = useState("");
   const [query, setQuery] = useState("");
+  const [order, setOrder] = useState("asc");
+  const [sortBy, setSortBy] = useState("");
 
-  console.log("q", query);
-
+  //console.log("q", query);
+  //console.log(sortType)
   const dispatch = useDispatch();
 
   const { addFolder } = useFolder();
@@ -42,6 +48,31 @@ export default function Header() {
     dispatch(setSearchData(searchResults));
   }, [query]);
 
+  useEffect(() => {
+    const sortFolder = (data, order, sortBy) => {
+      return [...data]
+        .map((folder) => ({
+          ...folder,
+          children: folder.children ? sortFolder(folder.children, order) : [],
+        }))
+        .sort((a, b) => {
+          if (sortBy === "size") {
+            return order === "asc" ? a.size.split(" ")[0] - b.size.split(" ")[0] : b.size.split(" ")[0] - a.size.split(" ")[0];
+          } else if (sortBy === "date") {
+            return order === "asc"
+              ? new Date(a.createDate) - new Date(b.createDate)
+              : new Date(b.createDate) - new Date(a.createDate);
+          } else {
+            return order === "asc"
+              ? a.name.localeCompare(b.name)
+              : b.name.localeCompare(a.name);
+          }
+        });
+    };
+    console.log("sd", sortFolder(folderData, order));
+    dispatch(setSortData(sortFolder(folderData, order, sortBy)));
+  }, [folderData, order, sortBy]);
+
   return (
     <div>
       <button onClick={() => setIsClickNewFolder(true)}>New Folder</button>
@@ -61,7 +92,13 @@ export default function Header() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         className=" bg-blue-50 text-black"
-      ></input>
+      />
+      <div className=" space-x-2">
+        <button onClick={() => setOrder("asc")}>asc</button>
+        <button onClick={() => setOrder("dsc")}>dsc</button>
+        <button onClick={() => setSortBy("date")}>date</button>
+        <button onClick={() => setSortBy("size")}>size</button>
+      </div>
     </div>
   );
 }
